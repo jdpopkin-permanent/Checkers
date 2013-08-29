2fb1a84058e00c014cc4f5835f2805e35630faef
Jackson Popkin <jdpopkin@gmail.com>
2013-08-29 10:27:56 -0400

    Adds initialize method to Checkers
    Also adds EOF to end of all files

--- checkers.rb
+++ checkers.rb
@@ -1,3 +1,12 @@
+require_relative "checker_piece.rb"
+require_relative "checkerboard.rb"
+
 class Checkers
+
+  attr_accessor :board
+
+  def initialize(start_board = nil)
+    @board = Board.new unless start_board
+  end
 
 end
