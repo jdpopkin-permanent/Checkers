93611e4cd88e0391a75cb34a6c40c72d71e7e008
Jackson Popkin <jdpopkin@gmail.com>
2013-08-29 10:24:54 -0400

    Rewrites movement methods to account for current location
    slide_moves and jump_moves now return an array of possible destinations
    after taking into account the Piece's starting position. Previously, they had
    only returned lists of possible offsets after a move.

--- checker_piece.rb
+++ checker_piece.rb
@@ -1,21 +1,42 @@
 class Piece
 
   attr_reader :color
-  attr_accessor :position, :king
+  attr_accessor :pos, :king
 
   def initialize(color, position)
     @color = color
-    @position = position
+    @pos = position
     @king = false
   end
 
-  def all_slides
-    white_slides = [[-1, 1], [1, 1]]
-    red_slides = [[-1, -1], [1, -1]]
+  def slide_moves
+    white_slides = [[-1, -1], [1, -1]]
+    red_slides = [[-1, 1], [1, 1]]
 
-    return white_slides + red_slides if self.king?
-    return white_slides if self.color == :white
-    return red_slides if self.color == :red
+    if self.king
+      slides = white_slides + red_slides
+    elsif self.color == :white
+      slides = white_slides
+    elsif self.color == :red
+      slides = red_slides
+    end
+
+    slides.map {|slide| [slide[0] + pos[0], slide[1] + pos[1]] }
+  end
+
+  def jump_moves
+    white_jumps = [[-2, -2], [2, -2]]
+    red_jumps = [[-2, 2], [2, 2]]
+
+    if self.king
+      jumps = white_jumps + red_jumps
+    elsif self.color == :white
+      jumps = white_jumps
+    elsif self.color == :red
+      jumps = red_jumps
+    end
+
+    jumps.map {|jump| [jump[0] + pos[0], jump[1] + pos[1]]}
   end
 
 
