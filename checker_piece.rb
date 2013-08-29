0f047087581a9df9f4c9e18bbdf0e6ee8c4c1c33
Jackson Popkin <jdpopkin@gmail.com>
2013-08-29 09:08:57 -0400

    Adds initialization and basic movement to Piece.

--- checker_piece.rb
+++ checker_piece.rb
@@ -1,3 +1,22 @@
 class Piece
 
+  attr_reader :color
+  attr_accessor :position, :king
+
+  def initialize(color, position)
+    @color = color
+    @position = position
+    @king = false
+  end
+
+  def all_slides
+    white_slides = [[-1, 1], [1, 1]]
+    red_slides = [[-1, -1], [1, -1]]
+
+    return white_slides + red_slides if self.king?
+    return white_slides if self.color == :white
+    return red_slides if self.color == :red
+  end
+
+
 end
